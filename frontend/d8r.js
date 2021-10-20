f876599df4aa3ed2e15b9017201971a852eb069e
OliverDavis <oliver.davis@bristol.ac.uk>
2021-10-20 10:35:07 +0100

    added dummy bar chart for model prediction

--- frontend/d8r.js
+++ frontend/d8r.js
@@ -233,12 +233,92 @@ let d8r = (function(d3){
     return(Math.sqrt(xs + ys));
   }
 
+  function getBarChart(height, width, div_id) {
+
+    // create 2 data_set
+    const data1 = [
+       {group: "A", value: 4},
+       {group: "B", value: 16},
+       {group: "C", value: 8}
+    ];
+
+    const data2 = [
+       {group: "A", value: 7},
+       {group: "B", value: 1},
+       {group: "C", value: 20},
+       {group: "D", value: 10}
+    ];
+
+    // set the dimensions and margins of the graph
+    const margin = {top: 50, right: 0, bottom: 50, left: 100};
+
+    width = width - margin.left - margin.right;
+    height = height - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    const svg = d3.select(div_id)
+      .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Initialize the X axis
+    const x = d3.scaleBand()
+      .range([ 0, width ])
+      .padding(0.2);
+    const xAxis = svg.append("g")
+      .attr("transform", `translate(0,${height})`)
+
+    // Initialize the Y axis
+    const y = d3.scaleLinear()
+      .range([ height, 0]);
+    const yAxis = svg.append("g")
+      .attr("class", "myYaxis")
+
+
+    // A function that create / update the plot for a given variable:
+    function update(data) {
+
+      // Update the X axis
+      x.domain(data.map(d => d.group))
+      xAxis.call(d3.axisBottom(x))
+
+      // Update the Y axis
+      y.domain([0, d3.max(data, d => d.value) ]);
+      yAxis.transition().duration(1000).call(d3.axisLeft(y));
+
+      // Create the u variable
+      var u = svg.selectAll("rect")
+        .data(data)
+
+      u
+        .join("rect") // Add a new rect for each new elements
+        .transition()
+        .duration(1000)
+          .attr("x", d => x(d.group))
+          .attr("y", d => y(d.value))
+          .attr("width", x.bandwidth())
+          .attr("height", d => height - y(d.value))
+          .attr("fill", "#69b3a2")
+    }
+
+    // Initialize the plot with the first dataset
+    update(data1)
+
+    return {
+      chart: svg,
+      update: update
+    }
+  }
+
   return {
     fixedNodeIDs: fixedNodeIDs,
     preprocess: preprocess,
     refreshNodeArray: refreshNodeArray,
     compileData: compileData,
     hexagonArray: hexagonArray,
-    dist: dist
+    dist: dist,
+    getBarChart: getBarChart
   };
 })(d3);
