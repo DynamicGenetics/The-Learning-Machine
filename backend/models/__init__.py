1a8c6143269f37da37083d533c7ce47194584cb0
leriomaggio <valerio.maggio@gmail.com>
2021-10-25 23:36:18 +0200

    New utility method to either return or instantiate DNN model

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -4,10 +4,23 @@ from .learning_machine import LearningMachine
 
 VGG_MODEL = "vgg"
 UNET_MODEL = "unet"
-MODELS_PROXY = {VGG_MODEL: VGGMachine(), UNET_MODEL: UNetMachine()}
+MODELS_PROXY = dict()
 
 
-def get_model(key: str) -> LearningMachine:
+def setup_learning_machine(model_key: str, pretrained: bool):
+    """
+    Utility method to initialise a new Learning Machine Model
+    """
+    if model_key == VGG_MODEL:
+        return VGGMachine(pretrained=pretrained)
+    if model_key == UNET_MODEL:
+        return UNetMachine(pretrained=pretrained)
+    raise ValueError(f"Model Key {model_key} is not currently supported")
+
+
+def get_model(
+    key: str, force_init: bool = False, pretrained: bool = False
+) -> LearningMachine:
     """
     Instantiate a Learning Machine Model given a key for Models Proxy.
 
@@ -15,6 +28,12 @@ def get_model(key: str) -> LearningMachine:
     ----------
     key : str
         A model proxy key
+    force_init: bool (default False)
+        If True, the model will be re-initialised before being returned.
+    pretrained: bool (default False)
+        This flag determines whether the re-initialised model should be returned
+        pre-trained or not.
+        NOTE: This parameter will only be used if `force_init` is True.
 
     Returns
     -------
@@ -28,9 +47,11 @@ def get_model(key: str) -> LearningMachine:
     ValueError
         Raised if input key is not valid. No default fall-back model implemented.
     """
-    try:
-        net = MODELS_PROXY[key]
-    except KeyError:
-        raise ValueError(f"Invalid Model Key: {key}")
-    else:
-        return net
+    net = MODELS_PROXY.get(key, None)
+    if net is None or force_init:
+        net = setup_learning_machine(key, pretrained=pretrained)
+        MODELS_PROXY[key] = net
+    print(
+        f"[LOG]: Loading Model: {net.name} (Pretrained: {'yes' if net.is_pretrained else 'no'})"
+    )
+    return net
