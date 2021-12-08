653c16ba1c447cb200239705147283454ff4380b
Valerio Maggio <leriomaggio@users.noreply.github.com>
2021-12-08 16:35:08 +0000

    New Metric calculated in each endpoint (accuracy) and returned in BackendRepose when appropriate.

--- backend/endpoints.py
+++ backend/endpoints.py
@@ -7,11 +7,12 @@ from datasets import Sample, get_dataset
 from models import get_model
 from models.learning_machine import Prediction
 from schemas import Node, EmotionLink, BackendResponse, Annotation
-from schemas import ImageBoard
-from settings import LEARNING_MACHINE_MODEL, DATASET_NAME
+from schemas import ImageBoard, Metric
+from settings import LEARNING_MACHINE_MODEL, DATASET_NAME, PRETRAINED
+from sklearn.metrics import accuracy_score
 
 
-def make_nodes(
+def _make_nodes(
     samples: Sequence[Sample], predicted_emotions: Prediction, classes: Sequence[str]
 ) -> List[Node]:
     nodes = list()
@@ -30,18 +31,37 @@ def make_nodes(
             id=sample.uuid,
             image=f"http://localhost:8000/faces/image/{sample.uuid}",
             links=links,
+            expected_emotion=sample.emotion,
+            expected_emotion_name=sample.emotion_label
         )
         nodes.append(node)
     return nodes
 
 
-async def faces(number_of_faces: int = 25, pretrained: bool = False):
+def _make_metrics(nodes: List[Node], classes: Sequence[str]) -> Sequence[Metric]:
+    metrics: Sequence[Metric] = list()
+    emotions_index = {emotion: i for i, emotion in enumerate(classes)}
+    y_true = [n.expected_emotion for n in nodes]
+    y_pred = [emotions_index[max(n.links, key=lambda l: l.value).target] for n in nodes]
+    acc = accuracy_score(y_true, y_pred)
+    metrics.append(Metric(value=acc))
+    return metrics
+
+
+def backend_response(
+    samples: Sequence[Sample], predicted_emotions: Prediction, classes: Sequence[str]
+) -> BackendResponse:
+    nodes = _make_nodes(samples, predicted_emotions, classes)
+    metrics = _make_metrics(nodes, classes)
+    return BackendResponse(nodes=nodes, metrics=metrics)
+
+
+async def faces(number_of_faces: int = 25, pretrained: bool = PRETRAINED):
     machine = get_model(LEARNING_MACHINE_MODEL, pretrained=pretrained)
     dataset = get_dataset(DATASET_NAME)
     samples = dataset.get_random_samples(k=number_of_faces)
     emotions = machine.predict(samples=samples)
-    nodes = make_nodes(samples, emotions, dataset.emotions)
-    response = BackendResponse(nodes=nodes)
+    response = backend_response(samples, emotions, dataset.emotions)
     return response.dict()
 
 
@@ -50,8 +70,7 @@ async def test_face(image_id: str):
     machine = get_model(LEARNING_MACHINE_MODEL)
     test_sample = dataset[image_id]
     emotions = machine.predict(samples=test_sample)
-    nodes = make_nodes([test_sample], emotions, dataset.emotions)
-    response = BackendResponse(nodes=nodes)
+    response = backend_response([test_sample], emotions, dataset.emotions)
     return response.dict()
 
 
@@ -83,8 +102,7 @@ async def annotate(annotation: Annotation):
     other_samples = [dataset[nid] for nid in annotation.current_nodes]
     other_samples += dataset.get_random_samples(k=annotation.new_nodes)
     updated_emotions = machine.predict(samples=other_samples)
-    nodes = make_nodes(other_samples, updated_emotions, dataset.emotions)
-    response = BackendResponse(nodes=nodes)
+    response = backend_response(other_samples, updated_emotions, dataset.emotions)
     return response.dict()
 
 
@@ -93,8 +111,7 @@ async def forget(current_nodes: ImageBoard, pretrained: bool = False):
     machine = get_model(LEARNING_MACHINE_MODEL, force_init=True, pretrained=pretrained)
     samples = [dataset[nid] for nid in current_nodes.nodes]
     predicted_emotions = machine.predict(samples=samples)
-    nodes = make_nodes(samples, predicted_emotions, dataset.emotions)
-    response = BackendResponse(nodes=nodes)
+    response = backend_response(samples, predicted_emotions, dataset.emotions)
     return response.dict()
 
 
@@ -104,8 +121,7 @@ async def discard_image(image_id: str):
     dataset.discard_sample(image_id)
     new_sample = dataset.get_random_samples(k=1)
     models_preds = machine.predict(samples=new_sample)
-    nodes = make_nodes(new_sample, models_preds, dataset.emotions)
-    response = BackendResponse(nodes=nodes)
+    response = backend_response(new_sample, models_preds, dataset.emotions)
     return response.dict()
 
 
