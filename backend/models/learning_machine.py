93f2b3685f8d1645e3a3064d58f995d2125fb0f3
Valerio Maggio <leriomaggio@users.noreply.github.com>
2022-03-02 14:48:27 +0000

    new VGG13 (FER adaptation) model
    New VGG13-fer-adapted model as per original FERPlus paper.
    The model is currently in training - and it is ready to switch to the new FERPlus dataset.
    Meanwhile, the whole infrastructure with the backend is in place and working.

--- backend/models/learning_machine.py
+++ backend/models/learning_machine.py
@@ -2,22 +2,33 @@
 LearningMachine Abstract base class definition
 """
 
-from typing import Any, NoReturn, Sequence, Tuple
-from nptyping import NDArray
+import os
+from pathlib import Path
+
 from numpy import float32 as float32
-import torch
+from numpy import log as np_log
 from abc import abstractmethod, ABC
+
+import torch
 from torch import nn, optim, Tensor
 from torch.utils.data.dataloader import default_collate
-from pathlib import Path
-import os
-from torchvision.transforms import ToTensor
+
+from torchvision.transforms import ToTensor, Compose, Lambda
 from torchvision.datasets.utils import download_url
-from datasets import Sample
+
+# Typing
+from typing import Any, NoReturn, Sequence, Tuple, Optional
 from typing import Callable, Union, Dict, Optional
+from nptyping import NDArray
 from PIL.Image import Image as PILImage
 
-# Types
+# Models
+from .unet import Unet
+from .vgg_fer import VGGFERNet
+from .vgg import VGG13Net
+
+from datasets import Sample
+
 ModelOutput = Union[Tensor, Tuple[Tensor, Tensor]]
 Prediction = NDArray[
     (
@@ -27,7 +38,9 @@ Prediction = NDArray[
     float32,
 ]
 TransformerType = Callable[[Union[Sequence[Callable], PILImage, Tensor]], Tensor]
-StateDictType = Union[Dict[str, Tensor], Dict[str, Tensor]]
+StateDictType = (
+    "OrderedDict[str, Tensor]"  # Union[Dict[str, Tensor], Dict[str, Tensor]]
+)
 
 BASE_FOLDER = Path(os.path.dirname(os.path.abspath(__file__)))
 TORCH_DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -199,3 +212,163 @@ class LearningMachine(ABC):
 
     def __call__(self, samples: Sequence[Sample]) -> Prediction:
         return self.predict(samples=samples)
+
+
+class UNetMachine(LearningMachine):
+    """Unet-based Learning Machine"""
+
+    def __init__(self, loss_reco_weight: float = 0.3, pretrained: bool = False):
+        super(UNetMachine, self).__init__(pretrained=pretrained)
+        self.loss_reco_coeff = loss_reco_weight
+        self._reconstruction_criterion, self._prediction_criterion = self._criterion
+
+    @property
+    def checkpoint(self) -> Path:
+        return self.CHECKPOINTS_FOLDER / "unet_learning_machine_nodecay_aug.pt"
+
+    @property
+    def weights_urls(self) -> Tuple[str, str]:
+        return (
+            "https://www.dropbox.com/s/nctn4x49t2xf6sq/"
+            + "unet_learning_machine_nodecay_aug.pt?dl=1",
+            "dbbd8866c5c6c7497feae735dd1513ce",
+        )
+
+    def _load_model(self):
+        model = Unet()
+        if self._pretrained:
+            model.load_state_dict(self.weights)
+        return model
+
+    def _init_optimiser(self):
+        return optim.Adam(self.model.parameters(), lr=0.0001)
+
+    def _init_criterion(self) -> Tuple[nn.Module, nn.Module]:
+        reco_criterion = nn.MSELoss(reduction="mean")
+        pred_criterion = nn.CrossEntropyLoss()
+        return reco_criterion, pred_criterion
+
+    @property
+    def criterion(self) -> Tuple[nn.Module, nn.Module]:
+        if self._criterion is None:
+            (
+                self._reconstruction_criterion,
+                self._prediction_criterion,
+            ) = self._init_criterion()
+            self._criterion = (
+                self._reconstruction_criterion,
+                self._prediction_criterion,
+            )
+        return self._criterion
+
+    @property
+    def reconstruction_criterion(self):
+        return self._reconstruction_criterion
+
+    @property
+    def prediction_criterion(self):
+        return self._prediction_criterion
+
+    @property
+    def name(self) -> str:
+        return "UNet"
+
+    def _model_call(self, batch: Sequence[Sample]) -> Tuple[Tensor, Tensor]:
+        return self.model(batch)
+
+    def _calculate_loss(
+        self,
+        labels: Tensor,
+        model_output: ModelOutput,
+        input_batch: Optional[Tensor] = None,
+    ) -> Tensor:
+        reco_images, emotions_logits = model_output
+        loss_reco = self.reconstruction_criterion(reco_images, input_batch)
+        loss_pred = self.prediction_criterion(emotions_logits, labels)
+        loss = (self.loss_reco_coeff * loss_reco) + (
+            1.0 - self.loss_reco_coeff
+        ) * loss_pred
+        return loss
+
+    @staticmethod
+    def _get_model_emotion_predictions(model_output: ModelOutput) -> Prediction:
+        reco_images, emotions_logits = model_output
+        return emotions_logits.detach().cpu()
+
+
+class VGGMachine(LearningMachine):
+    """VGG-based Learning Machine"""
+
+    def __init__(self, pretrained: bool = False) -> None:
+        super(VGGMachine, self).__init__(pretrained=pretrained)
+
+    @staticmethod
+    def _set_transformer() -> TransformerType:
+        def _convert_rgb(img: PILImage) -> PILImage:
+            return img.convert("RGB")
+
+        return Compose([Lambda(_convert_rgb), ToTensor()])
+
+    @property
+    def checkpoint(self) -> Path:
+        return self.CHECKPOINTS_FOLDER / "vgg_learning_machine_overfitting.pt"
+
+    @property
+    def weights_urls(self) -> Tuple[str, str]:
+        return (
+            "https://www.dropbox.com/s/2q68kitijwona2l/vgg_learning_machine_overfitting.pt?dl=1",
+            "e76c032150d9762e94a6b94b3d5c2b9d",
+        )
+
+    def _init_optimiser(self) -> optim.Optimizer:
+        return optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9)
+
+    def _init_criterion(self) -> nn.Module:
+        return nn.CrossEntropyLoss()
+
+    def _load_model(self) -> nn.Module:
+        model = VGG13Net(pretrained=False, freeze=False)
+        model.load_state_dict(self.weights)
+        return model
+
+
+class VGGFERMachine(LearningMachine):
+    """VGG-based Learning Machine"""
+
+    def __init__(self, pretrained: bool = False) -> None:
+        super(VGGFERMachine, self).__init__(pretrained=pretrained)
+
+    @staticmethod
+    def _set_transformer() -> TransformerType:
+        def _convert_rgb(img: PILImage) -> PILImage:
+            return img.convert("RGB")
+
+        return Compose([Lambda(_convert_rgb), ToTensor()])
+
+    @property
+    def checkpoint(self) -> Path:
+        return self.CHECKPOINTS_FOLDER / "TBD"
+
+    @property
+    def weights_urls(self) -> Tuple[str, str]:
+        return (
+            "TBD",
+            "TBD",
+        )
+
+    def _init_optimiser(self) -> optim.Optimizer:
+        momentum = -32 / np_log(0.9)  # -minibatch_size/log(0.9)
+        return optim.SGD(self.model.parameters(), lr=0.001, momentum=momentum)
+
+    def _init_criterion(self) -> nn.Module:
+        return nn.CrossEntropyLoss()
+
+    def _load_model(self) -> nn.Module:
+        model = VGGFERNet(in_channels=3)
+        # if self._pretrained:
+        #     model.load_state_dict(self.weights)
+        return model
+
+    @property
+    def name(self) -> str:
+        return "VGG"
---
+++ backend/models/vgg_fer.py
@@ -0,0 +1,71 @@
+import torch
+from torch import nn
+from collections import OrderedDict
+
+
+class VGGFERNet(nn.Module):
+    def __init__(self, in_channels: int = 1, n_classes: int = 7):
+        super(VGGFERNet, self).__init__()
+
+        self.conv1 = self._block(in_channels, 64, name="conv1")
+        self.conv2 = self._block(64, 128, name="conv2")
+
+        self.conv3 = self._block(128, 256, name="conv3", n_conv_layers=3)
+        self.conv4 = self._block(256, 256, name="conv4", n_conv_layers=3)
+
+        self.classifier = nn.Sequential(
+            OrderedDict(
+                [
+                    ("cl_fc_1", nn.Linear(256 * 3 * 3, 1024)),
+                    ("cl_relu_1", nn.ReLU(inplace=True)),
+                    ("cl_drop_1", nn.Dropout(0.5)),
+                    ("cl_fc_2", nn.Linear(1024, 1024)),
+                    ("cl_relu_2", nn.ReLU(inplace=True)),
+                    ("cl_drop_2", nn.Dropout(0.5)),
+                    ("output", nn.Linear(1024, n_classes)),
+                ]
+            )
+        )
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
+    @staticmethod
+    def _block(in_channels: int, features: int, name: str, n_conv_layers: int = 2):
+
+        layers = list()
+        for i in range(n_conv_layers):
+            layers.append(
+                (
+                    name + f"conv{i+1}",
+                    nn.Conv2d(
+                        in_channels=in_channels,
+                        out_channels=features,
+                        kernel_size=(3, 3),
+                        padding=1,
+                        bias=False,
+                    ),
+                )
+            )
+            layers.append((name + f"relu{i+1}", nn.ReLU(inplace=True)))
+            in_channels = features
+        layers.append(
+            (
+                name + "pool",
+                nn.MaxPool2d(kernel_size=(2, 2)),
+            )
+        )
+        layers.append(
+            (
+                name + "dropout",
+                nn.Dropout2d(p=0.25),
+            )
+        )
+
+        return nn.Sequential(OrderedDict(layers))
